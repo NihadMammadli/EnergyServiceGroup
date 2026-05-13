5b7b6b2a4329e5519eb4d54be5fbb6dd38c067ee
NihadMammadli <nihadmammadli03@gmail.com>
2026-05-13 14:19:09 +0400

    fix: new changes and animations

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
@@ -5,6 +6,11 @@ import styles from './MainLayout.module.css';
 
 export function MainLayout() {
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.pathname]);
+
   return (
     <div className={styles.layout}>
       <Header />
